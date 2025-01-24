b37b43e0a81303df1d4a9c18161c69885edc3b8b
imytk <77153961+imytk@users.noreply.github.com>
2025-01-24 15:59:37 +0000

    Deploying to gh-pages from @ imytk/imytk.github.io@a2c52fb0bd77d8f86ad57b8374105d66e14dcdfe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -375,16 +375,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-my-website-is-still-undergoing-a-few-updates-please-bear-with-me",
+          title: 'My website is still undergoing a few updates. Please bear with me. :)...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
